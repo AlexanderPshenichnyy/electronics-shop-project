e7882d4a3ba74081896a2bed40dbb0e75bc53926
Alexander Pshenichnyy <ter.anti@mail.ru>
2023-08-26 18:59:54 +0300

    Fixed the class and functions

--- src/item.py
+++ src/item.py
@@ -1,30 +1,34 @@
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
-    all = []
+	"""
+	Класс для представления товара в магазине.
+	"""
+	pay_rate = 1.0
+	all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+	def __init__(self, name: str, price: float, quantity: int) -> None:
+		"""
+		Создание экземпляра класса item.
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+		:param name: Название товара.
+		:param price: Цена за единицу товара.
+		:param quantity: Количество товара в магазине.
+		"""
+		self.name = name
+		self.price = price
+		self.quantity = quantity
+		self.all.append(self)
 
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+	def calculate_total_price(self) -> float:
+		"""
+		Рассчитывает общую стоимость конкретного товара в магазине.
 
-        :return: Общая стоимость товара.
-        """
-        pass
+		:return: Общая стоимость товара.
+		"""
+		return self.price * self.quantity
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+	def apply_discount(self) -> float:
+		"""
+		Применяет установленную скидку для конкретного товара.
+		"""
+		self.price *= self.pay_rate
+		return self.price
