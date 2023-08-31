4bd701c7f568fce971040e5ae6da3498ec623c98
Alexander Pshenichnyy <ter.anti@mail.ru>
2023-08-31 09:03:55 +0300

    Update item.py - added new functional Update main.py - changed comments indent Update test_item.py - added tests Add test_items.csv - for tests

--- src/item.py
+++ src/item.py
@@ -1,9 +1,14 @@
+import csv
+import os
+
+
 class Item:
 	"""
 	Класс для представления товара в магазине.
 	"""
 	pay_rate = 1.0
 	all = []
+	CSV_PATH = os.path.join('../src/items.csv')			# Глобальный путь к файлу items.csv
 
 	def __init__(self, name: str, price: float, quantity: int) -> None:
 		"""
@@ -13,10 +18,32 @@ class Item:
 		:param price: Цена за единицу товара.
 		:param quantity: Количество товара в магазине.
 		"""
-		self.name = name
+		self.__name = name
 		self.price = price
 		self.quantity = quantity
-		self.all.append(self)
+
+	@property
+	def name(self):
+		return self.__name
+
+	@name.setter
+	def name(self, name):
+		"""
+		Обрезает название до 10 символов
+		"""
+		self.__name = name[:10]
+
+	@classmethod
+	def instantiate_from_csv(cls):
+		Item.all.clear()							# Очистка списка Item.all
+		with open(cls.CSV_PATH, encoding="cp1251") as csvfile:
+			reader = csv.DictReader(csvfile)
+			for row in reader:
+				name = row["name"]
+				price = int(row["price"])
+				quantity = int(row["quantity"])
+				item = cls(name, price, quantity)  	# Создание экземпляра класса Item
+				cls.all.append(item)  				# Добавление экземпляра в список all
 
 	def calculate_total_price(self) -> float:
 		"""
@@ -32,3 +59,10 @@ class Item:
 		"""
 		self.price *= self.pay_rate
 		return self.price
+
+	@staticmethod
+	def string_to_number(string):
+		"""
+		Конвертирует строку или число с плавающей точкой в значение типа int
+		"""
+		return int(float(string))
--- tests/test_item.py
+++ tests/test_item.py
@@ -7,8 +7,7 @@ from src.item import Item
 @pytest.mark.parametrize('name, price, quantity, expected', [
 	("Смартфон", 10000, 20, 200000),
 	("Ноутбук", 20000, 5, 100000)
-]
-						)
+])
 def test_calculate_total_price(name, price, quantity, expected):
 	items = Item(name, price, quantity)
 	assert items.calculate_total_price() == expected
@@ -26,5 +25,30 @@ def test_apply_discount(name, price, quantity, expected):
 def test_all_items():
 	item1 = Item("Смартфон", 10000, 20)
 	item2 = Item("Ноутбук", 20000, 5)
-	assert item1, item2 == ['<src.item.Item object at 0x000002932B8E88D0>',
-							'<src.item.Item object at 0x000002932B929450>']
+	assert item1, item2 == [
+		'<src.item.Item object at 0x000002932B8E88D0>',
+		'<src.item.Item object at 0x000002932B929450>'
+	]
+
+
+def test_instantiate_from_csv():
+	Item.all = []
+	Item.CSV_PATH = 'test_items.csv'
+	Item.instantiate_from_csv()
+	assert len(Item.all) == 5
+	assert Item.all[0].name == 'Смартфон'
+
+
+def test_name_setter():
+	item = Item("Смартфон", 10000, 20)
+	assert item.name == 'Смартфон'
+	item.name = 'СуперСмартфон'
+	assert item.name == 'СуперСмарт'
+	item.name = ''
+	assert item.name == ''
+
+
+def test_string_to_number():
+	assert Item.string_to_number('5') == 5
+	assert Item.string_to_number('5.0') == 5
+	assert Item.string_to_number('5.5') == 5
