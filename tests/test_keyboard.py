ce79aa6bfcb38d4088981aa813a939bc3f3fe991
Alexander Pshenichnyy <ter.anti@mail.ru>
2023-09-11 21:43:30 +0300

    Added tests for keyboard.py

---
+++ tests/test_keyboard.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.keyboard import Keyboard, MixinLang
+
+
+@pytest.fixture
+def keyboard():
+	kb1 = Keyboard('Dark Project KD87A', 9600, 5)
+	return kb1
+
+
+def test_keyboard_language(keyboard):
+	assert keyboard.language == 'EN'
+
+
+def test_change_lang(keyboard):
+	keyboard.__language = 'EN'
+	keyboard.change_lang()
+	assert keyboard.language == 'RU'
