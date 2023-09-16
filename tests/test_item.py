fc8884290b2247f8e5aef0e83ee28f62200aba33
Alexander Pshenichnyy <ter.anti@mail.ru>
2023-09-16 09:47:52 +0300

    Added tests for custom class

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,7 +1,14 @@
 # """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
-
+import json
+import os, pytest, csv
 from src.item import Item
+from src.exception import InstantiateCSVError
+
+
+@pytest.fixture
+def items():
+	item = Item("Смартфон", 10000, 20)
+	return item
 
 
 @pytest.mark.parametrize('name, price, quantity, expected', [
@@ -22,7 +29,7 @@ def test_apply_discount(name, price, quantity, expected):
 	assert item1.apply_discount() == expected
 
 
-def test_all_items():
+def test_all_items(items):
 	item1 = Item("Смартфон", 10000, 20)
 	item2 = Item("Ноутбук", 20000, 5)
 	assert item1, item2 == [
@@ -38,13 +45,12 @@ def test_instantiate_from_csv():
 	assert Item.all[0].name == 'Смартфон'
 
 
-def test_name_setter():
-	item = Item("Смартфон", 10000, 20)
-	assert item.name == 'Смартфон'
-	item.name = 'СуперСмартфон'
-	assert item.name == 'СуперСмарт'
-	item.name = ''
-	assert item.name == ''
+def test_name_setter(items):
+	assert items.name == 'Смартфон'
+	items.name = 'СуперСмартфон'
+	assert items.name == 'СуперСмарт'
+	items.name = ''
+	assert items.name == ''
 
 
 def test_string_to_number():
@@ -53,11 +59,43 @@ def test_string_to_number():
 	assert Item.string_to_number('5.5') == 5
 
 
-def test_repr():
-	item = Item("Смартфон", 10000, 20)
-	assert repr(item) == "Item('Смартфон', 10000, 20)"
+def test_item_initialization(items):
+	assert items.name == "Смартфон"
+	assert items.price == 10000
+	assert items.quantity == 20
 
 
-def test_str():
-	item = Item("Смартфон", 10000, 20)
-	assert str(item) == 'Смартфон'
+def test_repr(items):
+	assert repr(items) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str(items):
+	assert str(items.name) == 'Смартфон'
+
+
+def test_cut_name(items):
+	items.name = 'Суперсмартфон'
+	assert items.name == "Суперсмарт"
+
+
+def test_raises_file_not_found():
+	with pytest.raises(FileNotFoundError, match='Отсутствует файл items.csv'):
+		Item.csv_path = 'items.csv'
+		Item.ABSOLUTE_PATH = os.path.abspath(Item.csv_path)
+		Item.instantiate_from_csv()
+
+
+def test_instantiate_csv_error():
+	with pytest.raises(InstantiateCSVError):
+		data = [
+			{'price': 100, 'quantity': 1},
+			{'price': 1010, 'quantity': 12}
+		]
+		with open('test_data.csv', 'w', encoding='cp1251', newline='') as data_csv:
+			row_names = ['price', 'quantity']
+			writing_to_a_file = csv.DictWriter(data_csv, fieldnames=row_names)
+			writing_to_a_file.writeheader()
+			writing_to_a_file.writerows(data)
+		Item.ABSOLUTE_PATH = 'test_data.csv'
+		Item.instantiate_from_csv()
+
